e2067b3c0480abe01a8a742556ef53e77ef29b00
yurii_bilous007 <yurii.b@elogic.com.ua>
2023-01-21 15:21:24 -0800

    setup new project

---
+++ app/design/frontend/Elogic/Default/registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * ElogicDefault theme
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Elogic/default', __DIR__);
